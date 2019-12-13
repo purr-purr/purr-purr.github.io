4475772001b23e1645d0db8dcb1fbe0ece3d16c4
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2019-12-13 16:12:35 +0200

    add scrollup button and animated

--- js/main.js
+++ js/main.js
@@ -3,11 +3,35 @@ $(document).ready(function(){
     $(".button_show-more").click(function(){
         $(".hidden").toggleClass("visible"); return false;
     });
-});;
+
 
 // Скрывает кнопку при развернутом портфолио
-$(document).ready(function(){
     $(".button_show-more").click(function(){
         $(".button_hidden").toggleClass("hidden"); return false;
     });
+
+
+// Для прокрутки при скроле
+    new WOW().init();
+
+
+// Плавная прокрутка к якорю
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+
+// Скрытие кнопки Scrollup
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.scrollup').fadeIn();
+                } else {
+                    $('.scrollup').fadeOut();
+                }
+    });
+
 });;
